0d11c97e428d1e6d5e0d6661f861a4c30592b724
Prakash251299 <prakashpratap2512@gmail.com>
2025-03-19 22:07:05 +0530

    Added a front screen to get the name of the user {which will be reflected with his location}

--- gps_tracker/src/main/java/com/example/gps_tracker/WebController.java
+++ gps_tracker/src/main/java/com/example/gps_tracker/WebController.java
@@ -3,13 +3,26 @@ package com.example.gps_tracker;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 public class WebController {
     @GetMapping("/")
     public String index() {
         return "index"; // This should return the Thymeleaf template
     }
+
+    @PostMapping("/submit-name")
+    public String submitName(@RequestParam("username") String username, Model model) {
+        model.addAttribute("username", username); // Pass username to map_screen.html
+        return "map_screen"; // Redirects to map_screen.html
+    }
+
 }
+
+
 
 
 
